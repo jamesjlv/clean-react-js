dfda0349ae2ce2d108848f3e3fdbfdcff199e9ab
jamesjlv <james.leal2@gmail.com>
2022-04-14 19:10:46 -0300

    feat: ensure login start with inputs with required state

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,12 +4,15 @@ import Context from "@/presentation/contexts/form/form-context";
 import Styles from "./form-status-styles.scss";
 
 export const FormStatus: React.FC = () => {
-  const { isLoading, errorMessage } = useContext(Context);
+  const {
+    state: { isLoading, errorMessage },
+    errorState: { main },
+  } = useContext(Context);
 
   return (
     <div className={Styles.loader} data-testid="error-wrap">
       {isLoading && <Spinner className={Styles.loading} />}
-      {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
+      {main && <span className={Styles.error}>{main}</span>}
     </div>
   );
 };
