b0056692d7396e5116465b0095605e31b4968135
jamesjlv <james.leal2@gmail.com>
2022-04-14 18:31:49 -0300

    test: ensure login will not render status on start

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { Login } from "./login";
 describe("Login page", () => {
-  test("", () => {
-    render(<Login />);
+  test("Should not render form status", () => {
+    const { getByTestId } = render(<Login />);
+    const errorWrap = getByTestId("error-wrap");
+    expect(errorWrap.childElementCount).toBe(0);
   });
 });
