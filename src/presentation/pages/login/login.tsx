954f3fda4e8ffbbeb8aa1fa89fa07d511ca6b2bd
jamesjlv <james.leal2@gmail.com>
2022-04-14 11:29:14 -0300

    refactor: create a export file in components folder

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Spinner } from "@/presentation/components/spinner/spinner";
+import {
+  LoginHeader,
+  FormStatus,
+  Footer,
+  Input,
+} from "@/presentation/components";
 
 import Styles from "./login-styles.scss";
-
-import { LoginHeader } from "@/presentation/components/login-header/login-header";
-import { Footer } from "@/presentation/components/footer/footer";
-import { Input } from "@/presentation/components/input/input";
-import { FormStatus } from "@/presentation/components/form-status/form-status";
 
 export const Login: React.FC = () => {
   return (
