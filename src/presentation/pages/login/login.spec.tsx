85d4770c96b9188ad4f78455b5da97872a6cb9e6
jamesjlv <james.leal2@gmail.com>
2022-04-14 20:43:27 -0300

    test: ensure login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,19 +1,39 @@
 import React from "react";
-import { render, RenderResult } from "@testing-library/react";
+import {
+  cleanup,
+  fireEvent,
+  render,
+  RenderResult,
+} from "@testing-library/react";
 import { Login } from "./login";
+import { Validation } from "@/presentation/protocols/validation";
 
 type SutTypes = {
   sut: RenderResult;
+  validationSpy: ValidationSpy;
 };
 
+class ValidationSpy implements Validation {
+  errorMessage: string;
+  input: object;
+  validate(input: object): string {
+    this.input = input;
+    return this.errorMessage;
+  }
+}
+
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />);
+  const validationSpy = new ValidationSpy();
+  const sut = render(<Login validation={validationSpy} />);
   return {
     sut,
+    validationSpy,
   };
 };
 
 describe("Login page", () => {
+  afterEach(cleanup);
+
   test("Should start with initial state", () => {
     const {
       sut: { getByTestId },
@@ -22,11 +42,33 @@ describe("Login page", () => {
     expect(errorWrap.childElementCount).toBe(0);
     const submit = getByTestId("submit") as HTMLButtonElement;
     expect(submit.disabled).toBe(true);
-    const emailStatus = getByTestId("email");
+    const emailStatus = getByTestId("emailStatus");
     expect(emailStatus.title).toBe("Campo obrigatório");
     expect(emailStatus.textContent).toBe("🔴");
-    const passwordStatus = getByTestId("password");
+    const passwordStatus = getByTestId("passwordStatus");
     expect(passwordStatus.title).toBe("Campo obrigatório");
     expect(passwordStatus.textContent).toBe("🔴");
+  });
+
+  test("Should call validation with correct values", () => {
+    const {
+      sut: { getByTestId },
+      validationSpy,
+    } = makeSut();
+    const email = getByTestId("email");
+    const password = getByTestId("password");
+    fireEvent.input(email, {
+      target: {
+        value: "any_email",
+      },
+    });
+    fireEvent.input(password, {
+      target: {
+        value: "any_password",
+      },
+    });
+    expect(validationSpy.input).toEqual({
+      email: "any_email",
+    });
   });
 });
