5d7d56dc7df1dee4fe3bd01b42b71bb27fbe4948
jamesjlv <james.leal2@gmail.com>
2022-04-14 23:20:36 -0300

    feat: prevent user to click in button if form is submiting

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -32,6 +32,9 @@ export const Login: React.FC<Props> = ({
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> {
     event.preventDefault();
+    if (state.isLoading) {
+      return;
+    }
     setState({ ...state, isLoading: true });
     await authentication.auth({ email: state.email, password: state.password });
   }
