eaae7cb5e03baa440107ab73e2cca44ab1a093ad
jamesjlv <james.leal2@gmail.com>
2022-04-14 22:16:43 -0300

    feat: add test id to spinner

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -6,7 +6,11 @@ type Props = React.HTMLAttributes<HTMLElement>;
 
 export const Spinner: React.FC<Props> = (props: Props) => {
   return (
-    <div {...props} className={[Styles.spinner, props.className].join(" ")}>
+    <div
+      {...props}
+      className={[Styles.spinner, props.className].join(" ")}
+      data-testid="spinner"
+    >
       <div></div>
       <div></div>
       <div></div>
