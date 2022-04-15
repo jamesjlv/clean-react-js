0ad0d26ac23f4287cebef2124d3499c4e9134e8c
jamesjlv <james.leal2@gmail.com>
2022-04-15 10:56:02 -0300

    test: create make sut

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,14 +1,18 @@
 import { RequiredFieldError } from "../errors";
 import { RequiredFieldValidation } from "./";
 import faker from "@faker-js/faker";
+
+const makeSut = (): RequiredFieldValidation =>
+  new RequiredFieldValidation("email");
+
 describe("RequiredFieldValidation", () => {
   test("Should return error if field is empty", () => {
-    const sut = new RequiredFieldValidation("email");
+    const sut = makeSut();
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
   });
   test("Should return falsy if field isnt empty", () => {
-    const sut = new RequiredFieldValidation("email");
+    const sut = makeSut();
     const error = sut.validate(faker.random.word());
     expect(error).toBeFalsy();
   });
