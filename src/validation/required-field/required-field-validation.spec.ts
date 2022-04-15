521d886860ed265e97eca8801babb8e7e4b68797
jamesjlv <james.leal2@gmail.com>
2022-04-15 10:52:23 -0300

    test: returns falsy if field its not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,15 @@
 import { RequiredFieldError } from "../errors";
 import { RequiredFieldValidation } from "./";
-
+import faker from "@faker-js/faker";
 describe("RequiredFieldValidation", () => {
   test("Should return error if field is empty", () => {
     const sut = new RequiredFieldValidation("email");
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
+  });
+  test("Should return falsy if field isnt empty", () => {
+    const sut = new RequiredFieldValidation("email");
+    const error = sut.validate(faker.random.word());
+    expect(error).toBeFalsy();
   });
 });
