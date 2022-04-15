1002e1fd97a323efd058a0aec2486a4468d6741b
jamesjlv <james.leal2@gmail.com>
2022-04-15 13:00:12 -0300

    test: move sut to his own method

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,14 +1,17 @@
 import { InvalidFieldError } from "@/validation/errors";
 import { MinLengthValidation } from "./min-length-validation";
 
+const makeSut = (minLength: number): MinLengthValidation =>
+  new MinLengthValidation("field", minLength);
+
 describe("MinLengthValidation", () => {
   test("Should return error if value lenght is less than required", () => {
-    const sut = new MinLengthValidation("field", 12);
+    const sut = makeSut(12);
     const error = sut.validate("123");
     expect(error).toEqual(new InvalidFieldError());
   });
   test("Should return falsy if value lenght is more than minLimit", () => {
-    const sut = new MinLengthValidation("field", 1);
+    const sut = makeSut(1);
     const error = sut.validate("1");
     expect(error).toBeFalsy();
   });
