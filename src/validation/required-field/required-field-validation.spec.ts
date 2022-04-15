1aa6fb95b75f08f195ae3cefe511be1bf43f3563
jamesjlv <james.leal2@gmail.com>
2022-04-15 10:50:02 -0300

    test: ensure required field validation returns an error if field is empty

---
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldError } from "../errors";
+import { RequiredFieldValidation } from "./";
+
+describe("RequiredFieldValidation", () => {
+  test("Should return error if field is empty", () => {
+    const sut = new RequiredFieldValidation("email");
+    const error = sut.validate("");
+    expect(error).toEqual(new RequiredFieldError());
+  });
+});
