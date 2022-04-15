b7001867585083f556d3e153bcf8b078074cbddb
jamesjlv <james.leal2@gmail.com>
2022-04-15 13:32:34 -0300

    test: return error if any validation fails

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -5,12 +5,13 @@ describe("ValidationComposite", () => {
   test("Should return error if any validation fails", () => {
     const fieldvalidationSpy = new FieldValidationSpy("any_field");
     const fieldvalidationSpy2 = new FieldValidationSpy("any_field");
-    fieldvalidationSpy.error = new Error("any_error_message");
+    fieldvalidationSpy.error = new Error("firstError");
+    fieldvalidationSpy2.error = new Error("secondError");
     const sut = new ValidationComposite([
       fieldvalidationSpy,
       fieldvalidationSpy2,
     ]);
     const error = sut.validate("any_field", "any_value");
-    expect(error).toBe("any_error_message");
+    expect(error).toBe("firstError");
   });
 });
