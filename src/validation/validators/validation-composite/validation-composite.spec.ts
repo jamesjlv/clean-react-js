9854d28dd07422c2406b9d8ddf62e14bc6263dac
jamesjlv <james.leal2@gmail.com>
2022-04-15 13:43:30 -0300

    test: ensure validation composite returns falsy if fieldValue is valid

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -6,10 +6,10 @@ type SutTypes = {
   fieldValidationsSpy: FieldValidationSpy[];
 };
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationSpy = [
-    new FieldValidationSpy("any_field"),
-    new FieldValidationSpy("any_field"),
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName),
   ];
 
   const sut = new ValidationComposite(fieldValidationSpy);
@@ -22,11 +22,18 @@ const makeSut = (): SutTypes => {
 
 describe("ValidationComposite", () => {
   test("Should return error if any validation fails", () => {
-    const { sut, fieldValidationsSpy } = makeSut();
+    const fieldName = faker.database.column();
+    const { sut, fieldValidationsSpy } = makeSut(fieldName);
     const errorMessage = faker.random.words();
     fieldValidationsSpy[0].error = new Error(errorMessage);
-    fieldValidationsSpy[1].error = new Error(errorMessage);
-    const error = sut.validate("any_field", "any_value");
+    fieldValidationsSpy[1].error = new Error(faker.random.words());
+    const error = sut.validate(fieldName, faker.random.word());
     expect(error).toBe(errorMessage);
+  });
+  test("Should return falsy if any validation fails", () => {
+    const fieldName = faker.database.column();
+    const { sut } = makeSut(fieldName);
+    const error = sut.validate(fieldName, faker.random.word());
+    expect(error).toBeFalsy();
   });
 });
