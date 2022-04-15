0f2c876a0e097313487900d21f087c69e4841fa9
jamesjlv <james.leal2@gmail.com>
2022-04-15 12:55:16 -0300

    feat: ensure field value has more caracters than the min limit

---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,9 @@
+import { InvalidFieldError } from "@/validation/errors";
+import { FieldValidation } from "@/validation/protocols";
+
+export class MinLengthValidation implements FieldValidation {
+  constructor(readonly field: string, private readonly minLenght: number) {}
+  validate(value: string): Error {
+    return value.length < this.minLenght ? new InvalidFieldError() : null;
+  }
+}
